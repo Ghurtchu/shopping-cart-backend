36188e4b8ee52cd10b58be2bc3893fe3e9cfb3f0
Anzori Ghurtchumelia <anzori.gurchumelia@gau.edu.ge>
2023-02-25 18:07:38 +0400

    write initial version of Checkout program

--- shop/src/main/scala/com/ghurtchu/Checkout.scala
+++ shop/src/main/scala/com/ghurtchu/Checkout.scala
@@ -1,7 +1,38 @@
 package com.ghurtchu
 
-final case class Checkout[F[_]](
+import cats.MonadThrow
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import cats.data.NonEmptyList
+import com.ghurtchu.Checkout.EmptyCartError
+import com.ghurtchu.order.OrderId
+import com.ghurtchu.payment.Payment
+import com.ghurtchu.user.UserId
+
+import scala.util.control.NoStackTrace
+
+final case class Checkout[F[_]: MonadThrow](
   paymentClient: PaymentClient[F],
   shoppingCart: ShoppingCart[F],
   orders: Orders[F],
-)
+) {
+
+  def process(userId: UserId, card: Any): F[OrderId] =
+    for {
+      cart <- shoppingCart.get(userId)
+      items <- ensureNonEmpty(cart.items)
+      pid <- paymentClient.process(Payment(userId, cart.total, card))
+      oid <- orders.create(userId, pid, items, cart.total)
+      _ <- shoppingCart.delete(userId)
+    } yield oid
+
+  private def ensureNonEmpty[A](as: List[A]): F[NonEmptyList[A]] =
+    MonadThrow[F].fromOption(
+      NonEmptyList.fromList(as),
+      EmptyCartError,
+    )
+}
+
+object Checkout {
+  final case object EmptyCartError extends NoStackTrace
+}
